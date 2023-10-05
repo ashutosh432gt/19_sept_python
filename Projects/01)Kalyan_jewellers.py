ba09a1a6d1e502bb28795c3c4e76caf03beb5607
ashutosh432gt <ashutoshsingh17032001@gmail.com>
2023-10-05 09:46:50 +0530

    Python

--- Projects/01)Kalyan_jewellers.py
+++ Projects/01)Kalyan_jewellers.py
@@ -3,6 +3,12 @@ print()
 name=input("Enter Your Name :- ")
 gender=input("Enter your gender Male/Female :- ")
 age=int(input("Enter Your age :- "))
+#declaring variable for calculation of final bil
+purchase_value1=0  
+total_purchase=0
+total_amount1=0
+total_making_charges1=0
+discount1=0
 status=True      #Declaring variable for running loop or we can call it intitlization
 while status:    #Applying entry condition to run this loop repeatedly
 
@@ -16,7 +22,8 @@ while status:    #Applying entry condition to run this loop repeatedly
     z=500000
     total_making_charges=product_weight * making_charges  
     total_amount=purchase_value + total_making_charges
-    a=purchase_value/100
+    discount=0
+    
     print()
     #conditional statement applied if the user's gender is male
     if gender=="male" or gender=="Male":
@@ -24,236 +31,106 @@ while status:    #Applying entry condition to run this loop repeatedly
         if age>65:
             #conditional statement if the total amount of purchase is between 2 Lakh and 3 Lakh
             if total_amount>x and total_amount<y:
-                discount=a*20 #20% discount applied
-                print()
-                #Bill genration
-                print("---------------Kalyan Jewellers---------------")
-                print("Total Gold Purchase value      INR ",purchase_value)
-                print("Total Gold Making Charges    + INR ",total_making_charges)
-                print("                            -----------------")
-                print("Total amount                 = INR ",total_amount)
-                print("Discount 20%                 - INR ",discount)
-                print("                            -----------------")
-                print("Total Net Amount             = INR ",total_amount - discount)
+                discount=0.20*purchase_value #20% discount applied
+                
             #conditional statement if the total amount of purchase is between 3 Lakh and 5 Lakh    
             elif total_amount>y and total_amount<z:
-                discount=a*30 #30% discount applied
-                print()
-                #Bill generation
-                print("---------------Kalyan Jewellers---------------")
-                print("Total Gold Purchase value      INR ",purchase_value)
-                print("Total Gold Making Charges    + INR ",total_making_charges)
-                print("                            -----------------")
-                print("Total amount                 = INR ",total_amount)
-                print("Discount 30%                 - INR ",discount)
-                print("                            -----------------")
-                print("Total Net Amount             = INR ",total_amount - discount )
+                discount=0.30*purchase_value #30% discount applied
+                
             #conditional statement if the total amount of purchase is above 5 Lakh     
             elif total_amount>z:
-                discount=a*35 #35% discount applied
-                print()
-                print("---------------Kalyan Jewellers---------------")
-                print("Total Gold Purchase value      INR ",purchase_value)
-                print("Total Gold Making Charges    + INR ",total_making_charges)
-                print("                            -----------------")
-                print("Total amount                 = INR ",total_amount)
-                print("Discount 35%                 - INR ",discount)
-                print("                            -----------------")
-                print("Total Net Amount             = INR ",total_amount - discount )
+                discount=0.35*purchase_value  #35% discount applied
+                
             #conditional statement if the total amount of purchase is below 2 Lakh
             else:
-                print()
-                #Bill generation
-                print("---------------Kalyan Jewellers---------------")
-                print("Total Gold Purchase value      INR ",purchase_value)
-                print("Total Gold Making Charges    + INR ",total_making_charges)
-                print("                            -----------------")
-                print("Total amount                 = INR ",total_amount)
-                print("Discount  0%                 - INR     0")
-                print("                            -----------------")
-                print("Total Net Amount             = INR ",total_amount - 0 )
+                discount=0*purchase_value #0% discount applied
     
             #conditional statement for the male users below of 65 age    
         else: 
             #conditional statement if the total amount of purchase is between 2 Lakh and 3 Lakh   
             if total_amount>x and total_amount<y:
-                discount=a*10 #10% discount applied 
-                print()
-                #Bill generation
-                print("---------------Kalyan Jewellers---------------")
-                print("Total Gold Purchase value      INR ",purchase_value)
-                print("Total Gold Making Charges    + INR ",total_making_charges)
-                print("                            -----------------")
-                print("Total amount                 = INR ",total_amount)
-                print("Discount 10%                 - INR ",discount)
-                print("                            -----------------")
-                print("Total Net Amount             = INR ",total_amount - discount )
+                discount=0.10*purchase_value  #10% discount applied 
+                
+                
             #conditional statement if the total amount of purchase is between 3 Lakh and 5 Lakh    
             elif total_amount>y and total_amount<z:
-                discount=a*20 #20% discount applied
-                print()
-                #Bill generation
-                print("---------------Kalyan Jewellers---------------")
-                print("Total Gold Purchase value      INR ",purchase_value)
-                print("Total Gold Making Charges    + INR ",total_making_charges)
-                print("                            -----------------")
-                print("Total amount                 = INR ",total_amount)
-                print("Discount 20%                 - INR ",discount)
-                print("                            -----------------")
-                print("Total Net Amount             = INR ",total_amount - discount )
+                discount=0.20*purchase_value  #20% discount applied
+                
             #conditional statement if the total amount of purchase is above 5 Lakh    
             elif total_amount>z:
-                discount=a*25 #25% discount applied
-                print()
-                #Bill generation
-                print("---------------Kalyan Jewellers---------------")
-                print("Total Gold Purchase value      INR ",purchase_value)
-                print("Total Gold Making Charges    + INR ",total_making_charges)
-                print("                            -----------------")
-                print("Total amount                 = INR ",total_amount)
-                print("Discount 25%                 - INR ",discount)
-                print("                            -----------------")
-                print("Total Net Amount             = INR ",total_amount - discount )
+                discount=0.25*purchase_value   #25% discount applied
+                
             #conditional statement if the total amount of purchase is below 2 Lakh 
             else:
-                print()
-                #Bill generation
-                print("---------------Kalyan Jewellers---------------")
-                print("Total Gold Purchase value      INR ",purchase_value)
-                print("Total Gold Making Charges    + INR ",total_making_charges)
-                print("                            -----------------")
-                print("Total amount                 = INR ",total_amount)
-                print("Discount  0%                 - INR     0")
-                print("                            -----------------")
-                print("Total Net Amount             = INR ",total_amount - 0 )
+                discount=0*purchase_value #0% discount applied
     #conditional statement applied if the user's gender is Female        
     elif gender=="Female" or gender=="female": 
         #condtional statement for Female user if thier age is above 65 (nested)
         if age>65: 
             #conditional statement if the total amount of purchase is between 2 Lakh and 3 Lakh                  
             if total_amount>x and total_amount<y:
-                discount=a*25 #25% discount applied
-                print()
-                #Bill generation
-                print("---------------Kalyan Jewellers---------------")
-                print("Total Gold Purchase value      INR ",purchase_value)
-                print("Total Gold Making Charges    + INR ",total_making_charges)
-                print("                            -----------------")
-                print("Total amount                 = INR ",total_amount)
-                print("Discount 25%                 - INR ",discount)
-                print("                            -----------------")
-                print("Total Net Amount             = INR ",total_amount - discount )
+                discount=0.25*purchase_value #25% discount applied
+                
             #conditional statement if the total amount of purchase is between 3 Lakh and 5 Lakh    
             elif total_amount>y and total_amount<z:
-                discount=a*35 #35% discount applied
-                print()
-                #Bill generation
-                print("---------------Kalyan Jewellers---------------")
-                print("Total Gold Purchase value      INR ",purchase_value)
-                print("Total Gold Making Charges    + INR ",total_making_charges)
-                print("                            -----------------")
-                print("Total amount                 = INR ",total_amount)
-                print("Discount 35%                 - INR ",discount)
-                print("                            -----------------")
-                print("Total Net Amount             = INR ",total_amount - discount )
+                discount=0.35*purchase_value #35% discount applied
+                
+                
             #conditional statement if the total amount of purchase is above 5 Lakh    
             elif total_amount>z:
-                discount=a*40
-                print()
-                #Bill generation
-                print("---------------Kalyan Jewellers---------------")
-                print("Total Gold Purchase value      INR ",purchase_value)
-                print("Total Gold Making Charges    + INR ",total_making_charges)
-                print("                            -----------------")
-                print("Total amount                 = INR ",total_amount)
-                print("Discount 40%                 - INR ",discount)
-                print("                            -----------------")
-                print("Total Net Amount             = INR ",total_amount - discount )
+                discount=0.40*purchase_value #40% discount applied
+                
+                
             #conditional statement if the total amount of purchase is below 2 Lakh 
             else:
-                print()
-                #Bill generation
-                print("---------------Kalyan Jewellers---------------")
-                print("Total Gold Purchase value      INR ",purchase_value)
-                print("Total Gold Making Charges    + INR ",total_making_charges)
-                print("                            -----------------")
-                print("Total amount                 = INR ",total_amount)
-                print("Discount  0%                 - INR     0")
-                print("                            -----------------")
-                print("Total Net Amount             = INR ",total_amount - 0 )
+                discount=0*purchase_value #0% discount applied
+                
             
         #conditional statement for the Female users below of 65 age    
         else: 
             #conditional statement if the total amount of purchase is between 2 Lakh and 3 Lakh   
             if total_amount>x and total_amount<y:
-                discount=a*15 #15% discount applied 
-                print()
-                #Bill generation
-                print("---------------Kalyan Jewellers---------------")
-                print("Total Gold Purchase value      INR ",purchase_value)
-                print("Total Gold Making Charges    + INR ",total_making_charges)
-                print("                            -----------------")
-                print("Total amount                 = INR ",total_amount)
-                print("Discount 15%                 - INR ",discount)
-                print("                            -----------------")
-                print("Total Net Amount             = INR ",total_amount - discount )
+                discount=0.15*purchase_value #15% discount applied 
+                
+                
             #conditional statement if the total amount of purchase is between 3 Lakh and 5 Lakh    
             elif total_amount>y and total_amount<z:
-                discount=a*25 #25% discount applied
-                print()
-                #Bill generation
-                print("---------------Kalyan Jewellers---------------")
-                print("Total Gold Purchase value      INR ",purchase_value)
-                print("Total Gold Making Charges    + INR ",total_making_charges)
-                print("                            -----------------")
-                print("Total amount                 = INR ",total_amount)
-                print("Discount 25%                 - INR ",discount)
-                print("                            -----------------")
-                print("Total Net Amount             = INR ",total_amount - discount )
+                discount=0.25*purchase_value #25% discount applied
+                
+                
             #conditional statement if the total amount of purchase is above 5 Lakh    
             elif total_amount>z:
-                discount=a*35 #35% discount applied
-                print()
-                #Bill generation
-                print("---------------Kalyan Jewellers---------------")
-                print("Total Gold Purchase value      INR ",purchase_value)
-                print("Total Gold Making Charges    + INR ",total_making_charges)
-                print("                            -----------------")
-                print("Total amount                 = INR ",total_amount)
-                print("Discount 35%                 - INR ",discount)
-                print("                            -----------------")
-                print("Total Net Amount             = INR ",total_amount - discount )        
+                discount=0.35*purchase_value #35% discount applied
+                
+                       
         
             #conditional statement if the total amount of purchase is below 2 Lakh 
             else:
-                print()
-                #Bill generation
-                print("---------------Kalyan Jewellers---------------")
-                print("Total Gold Purchase value      INR ",purchase_value)
-                print("Total Gold Making Charges    + INR ",total_making_charges)
-                print("                            -----------------")
-                print("Total amount                 = INR ",total_amount)
-                print("Discount  0%                 - INR     0")
-                print("                            -----------------")
-                print("Total Net Amount             = INR ",total_amount - 0 )     
-            
+                discount=0*purchase_value     #0%discount applied
+    #If users input is wrong        
     else:
         print()
         print("Invalid Input.Please choose gender between Male/Female")                       
     
+    #added calculation to calculate final bill after several purchases
+    purchase_value1+=purchase_value
+    total_making_charges1+=total_making_charges
+    total_amount1+=total_amount
+    discount1+=discount   
     #Asking users again if they want to purchase more items
+    print()
     choice=input("Do yo want to Continue Purchasing : Enter Yes Or No to Exit :- ").upper()
-
-    if choice=="YES" or choice=="Y":
-        status=True
-    else:
-        #Bill genration
-        print("---------------Kalyan Jewellers---------------")
-        print("Total Gold Purchase value      INR ",purchase_value)
-        print("Total Gold Making Charges    + INR ",total_making_charges)
-        print("                            -----------------")
-        print("Total amount                 = INR ",total_amount)
-        print("Discount 20%                 - INR ",discount)
-        print("                            -----------------")
-        print("Total Net Amount             = INR ",total_amount - discount)
-        status=False    
+    
+    if choice!="YES" and choice!="Y":
+        status=False
+        
+    
+#printing bill outside the loop for final calculation to avoid repetition     
+print("---------------Kalyan Jewellers---------------")
+print("Total Gold Purchase value      INR ",purchase_value1)
+print("Total Gold Making Charges    + INR ",total_making_charges1)
+print("                            -----------------")
+print("Total amount                 = INR ",total_amount1)
+print("Discount                     - INR ",discount1)
+print("                            -----------------")
+print("Total Net Amount             = INR ",total_amount1 - discount1 )        
